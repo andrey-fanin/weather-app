cec47965384c99d44d37bbf8640a125ba46e24e7
Andrey Fanin <andr.fanin16@yandex.ru>
2023-03-03 21:46:32 +0500

    fixed css rules to improve the widget experience

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import WeatherWidget from './components/WeatherWidget.vue'
-import './style.css'
-
-createApp(App).component('WeatherWidget', WeatherWidget).mount('#app')
